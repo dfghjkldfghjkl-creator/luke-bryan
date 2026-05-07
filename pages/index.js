19d3b350e55e7ad673b10766320be04adf8e9db6
dfghjkldfghjkl-creator <nwaforalexander58@gmail.com>
2026-05-07 14:13:50 +0100

    Add pages/index.js for Next.js homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to my website!</h1>
+      <p>This is deployed on Vercel with Next.js</p>
+    </div>
+  )
+}
